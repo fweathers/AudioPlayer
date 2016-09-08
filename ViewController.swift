cfc8d6970437615490a2ed85be363be3e3d9925b
Felicia Weathers <Fel.Weathers@gmail.com>
2016-09-08 01:05:04 -0400

    Set up basic audio player

---
+++ ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Felicia Weathers on 9/8/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let audioPath = Bundle.main.path(forResource: "mozart", ofType: "mp3")
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+            player.play()
+            
+        } catch {
+            
+            //process any errors
+            
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
